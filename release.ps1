de2006c66f72063df2b0646c320a819ab58318c4
duong.nb <48197320+NDiiong@users.noreply.github.com>
2024-01-29 07:41:22 -0800

    Update release.ps1

--- release.ps1
+++ release.ps1
@@ -1,2 +1,3 @@
 dotnet publish Flow.Launcher.Plugin.GuidConverter -c Release -r win-x64
 Compress-Archive -LiteralPath Flow.Launcher.Plugin.GuidConverter/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.GuidConverter/bin/GuidConverter.zip -Force
+
